4d81b8de9920af17db8adea896224e552b6775ea
Amoghchoudhary <84228409+Amoghchoudhary@users.noreply.github.com>
2021-12-29 17:46:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -149,3 +149,49 @@ function my_keydown(e) {
 
     }
 }
+
+function up(){
+
+ if(player_y >=0){
+ 
+ player_y = player_y - block_image_height;
+ console.log("block_image_height = " + block_image_height);
+ console.log("when Up arrow key is pressd, X = " + player_x + ", y = " + player_y);
+ canvas.remove(player_object);
+ player_update();
+ }
+}
+function down(){
+
+    if(player_y <=500){
+ 
+    player_y = player_y + block_image_height;
+    console.log("block_image_height = " + block_image_height);
+    console.log("when Down arrow key is pressd, X = " + player_x + ", y = " + player_y);
+    canvas.remove(player_object);
+    player_update();
+    }
+}
+    function left(){
+
+    if(player_x >0){
+ 
+        player_x = player_x - block_image_width;
+        console.log("block_image_width = " + block_image_width);
+        console.log("when Left arrow key is pressd, X = " + player_x + ", y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+        }
+    }
+        function right(){
+
+            if(player_x <=850){
+         
+                player_x = player_x + block_image_width;
+                console.log("block_image_width = " + block_image_width);
+                console.log("when Right arrow key is pressd, X = " + player_x + ", y = " + player_y);
+                canvas.remove(player_object);
+                player_update();
+                }
+        }
+        
